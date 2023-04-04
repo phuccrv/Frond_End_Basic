a8a27a789308f49cfa81fb83684969a4132d0150
PHUC NGUYEN <phuccrv@gmail.com>
2023-04-04 20:13:31 +0700

    hoàn thành

---
+++ Class/js/script.js
@@ -0,0 +1,8 @@
+var soNhapA = prompt("nhập số a");
+var soNhapB = prompt("nhập số b");
+
+if(soNhapA % soNhapB ==0){
+    console.log("số a bạn vừa nhập chia hết cho b");
+}else{
+    console.log("số a không chia hết cho b");
+}
